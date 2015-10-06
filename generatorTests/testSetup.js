70265f204368cd6a46359f7269935e994d5f05e3
tristanashley <tristan.ashley@codecomputerlove.com>
2015-10-06 17:14:55 +0100

    feat: Additions to test setup script

--- generatorTests/testSetup.js
+++ generatorTests/testSetup.js
@@ -5,16 +5,21 @@ var path = require('path');
 const PROJECT_DIRECTORY = path.join(__dirname, '..');
 const OS_TEMP_LOCATION = path.join(os.tmpdir(), 'node-static-site-generator-tests');
 
-console.log(PROJECT_DIRECTORY);
-console.log(OS_TEMP_LOCATION);
+console.log('1) Setting up environment for testing');
+console.log('2) Copying source files to OS temp directory');
 
-fs.copy(PROJECT_DIRECTORY, OS_TEMP_LOCATION, function (err) {
+fs.copy(PROJECT_DIRECTORY, OS_TEMP_LOCATION, { 
+    clobber: true,
+    filter: function(directory) { 
+        //exclude specific directories (mainly node_modules) from being copied over
+        return directory.indexOf('karma') === -1 && directory.indexOf('phantom') === -1 && directory.indexOf('express') === -1; 
+    }
+}, function (err) {
 
     if (err) {
-        console.error('Unable to copy source files');
+        console.error('3) Unable to copy source files');
         return console.error(err)
     }
 
-    console.log('Test setup complete');
-
+    console.log('3) Source files copied - Test setup complete');
 })

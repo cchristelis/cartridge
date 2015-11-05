a4fcdfc8e8d9e6a80c3920fce20d20f84d42a6dd
tawashley <tristan.ashley@codecomputerlove.com>
2015-11-05 14:53:18 +0000

    test: Update gulp build tests to assert against compiled files existing

--- generatorTests/test/gulpSpec.js
+++ generatorTests/test/gulpSpec.js
@@ -78,6 +78,49 @@ describe('As a dev', function() {
             pathToTest.should.not.be.a.path();
         });
 
+        it('the build folder should include the _client folder', function() {
+            var pathToTest = path.join(ROOT_DIR, 'build', '_client');
+            pathToTest.should.be.a.path();
+        });
+
+        it('The _client folder should include the styles folder', function() {
+            var pathToTest = path.join(ROOT_DIR, 'build', '_client', 'styles');
+            pathToTest.should.be.a.path();
+        });
+
+        it('The styles folder should include the compiled CSS files', function() {
+            var pathToTest = path.join(ROOT_DIR, 'build', '_client', 'styles');
+            var filePathToTest;
+
+            var files = [
+                'ie8.css',
+                'main.css'
+            ];
+
+            files.forEach(function(fileName) {
+                filePathToTest = path.join(pathToTest, fileName);
+                filePathToTest.should.be.a.file().and.not.empty;
+            });
+        });
+
+        it('The _client folder should include the scripts folder', function() {
+            var pathToTest = path.join(ROOT_DIR, 'build', '_client', 'scripts');
+            pathToTest.should.be.a.path();
+        });
+
+        it('The scripts folder should include the compiled JavaScript files', function() {
+            var pathToTest = path.join(ROOT_DIR, 'build', '_client', 'scripts');
+            var filePathToTest;
+
+            var files = [
+                'bundle-critical.js',
+                'bundle.js'
+            ];
+
+            files.forEach(function(fileName) {
+                filePathToTest = path.join(pathToTest, fileName);
+            });
+        });
     });
 
 });

f135586ede58458b3c58969e7905821435776de5
tawashley <tristan.ashley@codecomputerlove.com>
2015-11-13 10:47:15 +0000

    refactor: Clean up, remove uneeded variables

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,21 @@
+/* ============================================================ *\
+    SETUP
+\* ============================================================ */
+
 /*jslint node: true */
 'use strict';
 
 var gulp = require('gulp'),
-		plugins = require('gulp-load-plugins')(),
-    // sgc = require('gulp-sass-generate-contents'),
-    // postcss = require('gulp-postcss'),
-    // autoprefixer = require('autoprefixer-core'),
-    // imagemin = require('gulp-imagemin'),
-		// pngquant = require('imagemin-pngquant'),
-		// concat = require('gulp-concat'),
-		// uglify = require('gulp-uglify'),
-		// minifyCss = require('gulp-minify-css'),
-		// jshint = require('gulp-jshint'),
-		argv = require('yargs').argv,
-    // gulpif = require('gulp-if'),
-    runSeq = require('run-sequence'),
-		config = require('./_config/project.json'),
-        // templateDataJson = require('./_config/templateData.json'),
-        // templateHelpers = require('./_config/templateHelpers.js')(),
-		// jshintConfig = require('./_config/jshint.json'),
-		creds = require('./_config/creds.json'),
-		// itcss = require('./_config/itcss'),
-		destStyles = config.src + '/' + config.dirs.styles;
-		// sourcemaps   = require('gulp-sourcemaps'),
-		// handlebars = require('gulp-compile-handlebars'),
-		// pixrem = require('gulp-pixrem');
-		// rename = require('gulp-rename'),
-		// zip = require('gulp-zip'),
-        // browserSync = require('browser-sync'),
-        // nodemon = require('gulp-nodemon'),
-        // gulpCache = require('gulp-cached');
-        // handlebarsConfig = require('./_config/handlebars.json');
+var plugins = require('gulp-load-plugins')();
+var argv = require('yargs').argv;
+var runSeq = require('run-sequence');
+var onfig = require('./_config/project.json');
+var eds = require('./_config/creds.json');
+var stStyles = config.src + '/' + config.dirs.styles;
+
+/* ============================================================ *\
+    TASKS
+\* ============================================================ */
 
 require('./gulpTasks/styles.js')(gulp, config, argv, destStyles);
 require('./gulpTasks/scripts.js')(gulp, config, argv);
@@ -46,13 +31,14 @@ require('./gulpTasks/local-testing.js')(gulp, config);
     MAIN TASKS
 \* ============================================================ */
 
-
 gulp.task('watch:sass', function () {
 	gulpif(!argv.prod, gulp.watch([config.src + '/' + config.dirs.styles + '/**/*.scss', config.dirs.components + '/**/*.scss'], ['sass']));
 });
+
 gulp.task('watch:js', function () {
 	gulpif(!argv.prod, gulp.watch([config.src + '/' + config.dirs.scripts + '/**/*.js', config.dirs.components + '/**/*.js'], ['scripts']));
 });
+
 gulp.task('watch', function (cb) {
 	runSeq(['watch:sass', 'watch:js'], cb);
 });

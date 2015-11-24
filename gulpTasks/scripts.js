ea5fb7309402c0fdbc6d2cf064e75b708dbd4d96
Barney Scott <barney.scott@codecomputerlove.com>
2015-11-24 15:55:17 +0000

    Formatting improvements, and switching the logic on linting so that it runs on dev rather than production

--- gulpTasks/scripts.js
+++ gulpTasks/scripts.js
@@ -1,37 +1,44 @@
+'use strict';
+
 /* ============================================================ *\
     SCRIPTS JS / lint, concat and minify scripts
 \* ============================================================ */
 
-var gulpif = require('gulp-if');
-var jshint = require('gulp-jshint');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
+// Grunt dependencies
+var sourcemaps   = require('gulp-sourcemaps');
+var gulpif       = require('gulp-if')
+var concat       = require('gulp-concat');
 
-var jshintConfig = require('../_config/jshint.json')
+// Javascript dependencies
+var jshint       = require('gulp-jshint');
+var uglify       = require('gulp-uglify');
+
+// Config
+var jshintConfig = require('../_config/jshint.json');
 
 module.exports = function(gulp, config, argv) {
 
     gulp.task('scripts', function(){
         return gulp.src([config.src + '/' + config.dirs.scripts + '/**/*.js'])
-            .pipe(gulpif(argv.prod, jshint(jshintConfig))) //Default only
-        .pipe(concat('bundle.js'))
-        .pipe(gulpif(argv.prod, uglify())) //Production only
-        .pipe(gulp.dest(config.dest + '/' + config.dirs.scripts));
+            .pipe(gulpif(!argv.prod, jshint(jshintConfig))) //Default only
+            .pipe(concat('bundle.js'))
+            .pipe(gulpif(argv.prod, uglify())) //Production only
+            .pipe(gulp.dest(config.dest + '/' + config.dirs.scripts));
     });
 
     gulp.task('scripts:vendor', function(){
         return gulp.src([config.src + '/' + config.dirs.scripts + '/vendor/*.js'])
-            .pipe(gulpif(argv.prod, jshint(jshintConfig))) //Default only
-        .pipe(concat('bundle-critical.js'))
-        .pipe(gulpif(argv.prod, uglify())) //Production only
-        .pipe(gulp.dest(config.dest + '/' + config.dirs.scripts));
+            .pipe(gulpif(!argv.prod, jshint(jshintConfig))) //Default only
+            .pipe(concat('bundle-critical.js'))
+            .pipe(gulpif(argv.prod, uglify())) //Production only
+            .pipe(gulp.dest(config.dest + '/' + config.dirs.scripts));
     });
 
     gulp.task('scripts:ie', function(){
         return gulp.src([config.src + '/' + config.dirs.scripts + '/ie/*.js'])
-        .pipe(concat('ie.js'))
-        .pipe(gulpif(argv.prod, uglify())) //Production only
-        .pipe(gulp.dest(config.dest + '/' + config.dirs.scripts));
+            .pipe(concat('ie.js'))
+            .pipe(gulpif(argv.prod, uglify())) //Production only
+            .pipe(gulp.dest(config.dest + '/' + config.dirs.scripts));
     });
 
 }

076fca34e59b9ec45156a495d6f1fa3216fcfb91
tawashley <tristan.ashley@codecomputerlove.com>
2015-11-27 12:08:18 +0000

    refactor: Move logic into seperate functions

--- gulpTasks/new-component.js
+++ gulpTasks/new-component.js
@@ -14,34 +14,41 @@ module.exports = function(gulp, argv) {
     gulp.task('new-component', function (cb) {
 
         if(!argv.name) {
-            console.log('');
-            console.log('A component must have a name');
-            console.log('');
-            console.log('Example usage:')
-            console.log('gulp component --name header');
-            console.log('');
+            noNameGivenOutput();
         } else {
-
-            hbsFileName = argv.name + '.hbs';
-            scssFileName = argv.name + '.scss';
-            componentPath = path.join(partialsPath, argv.name);
-
-            fs.mkdirSync(componentPath);
-            console.log('');
-            console.log(chalk.green('✓ Folder "%s" created'), argv.name);
-
-            fs.writeFileSync(path.join(componentPath, hbsFileName), '<h1>' + argv.name +'</h1>', 'utf8');
-            console.log(chalk.green('✓ Handlebars file "%s" created'), hbsFileName);
-
-            fs.writeFileSync(path.join(componentPath, scssFileName), '//' + argv.name + ' styles', 'utf8');
-            console.log(chalk.green('✓ Sass file "%s" created'), scssFileName);
-            console.log('')
-            console.log(chalk.bold('Component files created in "%s"'), componentPath);
-            console.log('')
+            createComponentAssets();
         }
 
         cb();
 
     });
+
+    function noNameGivenOutput() {
+        console.log('');
+        console.log('A component must have a name');
+        console.log('');
+        console.log('Example usage:')
+        console.log('gulp component --name header');
+        console.log('');
+    }
+
+    function createComponentAssets() {
+        hbsFileName = argv.name + '.hbs';
+        scssFileName = argv.name + '.scss';
+        componentPath = path.join(partialsPath, argv.name);
+
+        fs.mkdirSync(componentPath);
+        console.log('');
+        console.log(chalk.green('✓ Folder "%s" created'), argv.name);
+
+        fs.writeFileSync(path.join(componentPath, hbsFileName), '<h1>' + argv.name +'</h1>', 'utf8');
+        console.log(chalk.green('✓ Handlebars file "%s" created'), hbsFileName);
+
+        fs.writeFileSync(path.join(componentPath, scssFileName), '//' + argv.name + ' styles', 'utf8');
+        console.log(chalk.green('✓ Sass file "%s" created'), scssFileName);
+        console.log('')
+        console.log(chalk.bold('Component files created in "%s"'), componentPath);
+        console.log('')
+    }
 
 }

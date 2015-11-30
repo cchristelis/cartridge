8201b54c9b2ba3eab1b52f74572d7cc583b86096
Daniel Furze <daniel@furzeface.com>
2015-11-30 16:06:58 +0000

    fix: update scripts path in Karma test config

--- karma.conf.js
+++ karma.conf.js
@@ -18,7 +18,7 @@ module.exports = function(config) {
         // list of files / patterns to load in the browser
         files: [
             'test/*Spec.js',
-            '_source/scripts/*.js'
+            '_source/scripts/**/*.js'
         ],
 
 
